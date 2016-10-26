6e03df4a5db478f72c38f6889113f68aad0f59c1
abdul.aziz <seeazee@gmail.com>
2016-10-26 19:49:29 +0100

    fixed test added mocks for dependency

--- src/test/java/com/retail/manager/test/ShopsControllerTests.java
+++ src/test/java/com/retail/manager/test/ShopsControllerTests.java
@@ -1,9 +1,14 @@
 package com.retail.manager.test;
 
 import com.retail.manager.config.MainApplication;
-import org.junit.Before;
-import org.junit.Test;
+import com.retail.manager.controller.ShopsController;
+import com.retail.manager.domain.Shop;
+import com.retail.manager.service.ShopService;
+import org.junit.*;
 import org.junit.runner.RunWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.SpringApplicationConfiguration;
 import org.springframework.http.MediaType;
@@ -11,16 +16,20 @@ import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.springframework.test.context.web.WebAppConfiguration;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
-import org.springframework.web.context.WebApplicationContext;
+
 
 import java.nio.charset.Charset;
+import java.util.HashSet;
+import java.util.Set;
 
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
-
+import static org.hamcrest.Matchers.*;
+import static org.mockito.Mockito.*;
 /**
  * Created by abdulaziz on 20/10/2016.
  */
@@ -30,27 +39,50 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 @WebAppConfiguration
 public class ShopsControllerTests {
 
-    @Autowired
-    private WebApplicationContext webApplicationContext;
-
     private MediaType contentType =
             new MediaType(MediaType.APPLICATION_JSON.getType(),
             MediaType.APPLICATION_JSON.getSubtype(), Charset.forName("utf8"));
 
+    @InjectMocks
+    private ShopsController shopsController;
+
     private MockMvc mockMvc;
+
+    @Mock
+    @Autowired
+    ShopService shopService;
 
     @Before
     public void setup() throws Exception {
-        this.mockMvc = MockMvcBuilders.webAppContextSetup(webApplicationContext).build();
+
+        // Process mock annotations
+        MockitoAnnotations.initMocks(this);
+
+        this.mockMvc =  MockMvcBuilders.standaloneSetup(shopsController).build();
 
     }
 
     @Test
     public void getShopsTest() throws Exception {
 
+        Set<Shop> data = new HashSet<>();
+        data.add(new Shop("shop-1",1001,"334"));
+        data.add(new Shop("shop-2",1002,"335"));
+
+        // define return value for method getShops()
+        when(shopService.getShops())
+                .thenReturn(data);
+
         mockMvc.perform(get("/api/v1/shops"))
                 .andExpect(status().isOk())
-                .andExpect(content().contentType(contentType));
+                .andExpect(content().contentType(contentType))
+                .andExpect(jsonPath("$", hasSize(2)))
+                .andExpect(jsonPath("$[0].shopName", is("shop-1")))
+                .andExpect(jsonPath("$[1].shopName", is("shop-2")));
+
+        verify(shopService, times(1)).getShops();
+        verifyNoMoreInteractions(shopService);
+
     }
 
 
@@ -72,7 +104,11 @@ public class ShopsControllerTests {
     @Test
     public void addShopValidTest() throws Exception {
 
-        String jsonData = "{\"shopName\": \"Shop231\", \"shopAddress\": {\"number\": 296, \"postcode\": 333}}";
+        Shop newShop = new Shop("Shop-3",296,"333");
+        when(shopService.addShop(newShop))
+                .thenReturn(true);
+
+        String jsonData = "{\"shopName\": \"Shop-3\", \"shopAddress\": {\"number\": 296, \"postcode\": 333}}";
         mockMvc.perform(post("/api/v1/shops")
                 .contentType(contentType).content(jsonData.getBytes()))
                 .andExpect(status().isCreated())
